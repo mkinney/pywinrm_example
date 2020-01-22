7d567b120a500524f1b3f46158e2b7f916ef2430
Mike Kinney <mike.kinney@gmail.com>
2020-01-22 10:58:54 -0800

    add some powershell examples

---
+++ run_ps_memory.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import winrm
+
+import winrm
+
+ps_script = """$strComputer = $Host
+Clear
+$RAM = WmiObject Win32_ComputerSystem
+$MB = 1048576
+
+"Installed Memory: " + [int]($RAM.TotalPhysicalMemory /$MB) + " MB" """
+
+s = winrm.Session('192.168.2.246:5985', auth=('vagrant', 'vagrant'))
+r = s.run_ps(ps_script)
+
+print("status_code:{}".format(r.status_code))
+
+print("output:\n", r.std_out.decode('utf-8'))
